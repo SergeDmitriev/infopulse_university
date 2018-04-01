120a76f3cf01be3b3c146b7295a62a7c18e0c8e1
s.dmitriev <s.dmitriev@e-tender.biz>
2018-04-01 18:10:38 +0300

    add lecture_1 and lecture_2 to git_studying_repository

---
+++ Lecture2/var_abilities.py
@@ -0,0 +1,33 @@
+x = b = 3
+
+a = [1,2,3,4,'5']
+a[0]='b'
+print(a)
+
+a1 = 1
+a2 = 2
+print(a == 2)
+
+b1 = b2 = 1
+print(b1 is b2)
+
+t = 'text'
+len(t)
+len(t[2])
+print(str(a))
+
+h = 'text'
+tuple5 = (1,2,3,4, h)
+print(h)
+
+u = ['text1', 'text2', 'text3']
+print(u)
+
+
+
+vat =2**10
+tax = 18
+tax/=100
+
+p = '100' * 5
+# 100100100100100
