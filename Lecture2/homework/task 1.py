58e1505fdc4afd6626e1d67b9e9b19e1d499b7eb
s.dmitriev <s.dmitriev@e-tender.biz>
2018-04-03 21:34:45 +0300

    task1.1

---
+++ Lecture2/homework/task 1.py
@@ -0,0 +1,11 @@
+task1 = input('Input text here: ')
+
+# 1.	Определите является ли строка записью числа. (Подсказка: ищите как это сделать с помощью методов строк)
+
+print('Task1 result:')
+if task1.isdigit():
+    print('Input row is a string')
+else:
+    print('Input row is NOT a string')
+
+# 2.	Посчитайте сколько у вас пробелов в строке.
