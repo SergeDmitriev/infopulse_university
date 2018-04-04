19c83cbcfdb7f9f10589e2833b89f9de10900ccd
s.dmitriev <s.dmitriev@e-tender.biz>
2018-04-04 09:59:30 +0300

    add task1 1.1 - 1.4

--- Lecture2/homework/task 1.py
+++ Lecture2/homework/task 1.py
@@ -1,11 +1,38 @@
-task1 = input('Input text here: ')
-
+text = input('Input text here: ')
+# print(dir(text))
 # 1.	Определите является ли строка записью числа. (Подсказка: ищите как это сделать с помощью методов строк)
 
-print('Task1 result:')
-if task1.isdigit():
-    print('Input row is a string')
+print('\n\nTask1 result:')
+if text.isdigit():
+    print('Input row is a set of numbers')
 else:
-    print('Input row is NOT a string')
+    print('Input row is NOT a set of numbers')
 
 # 2.	Посчитайте сколько у вас пробелов в строке.
+print('\n\nTask2 result:')
+
+print("Space quantity: {0}".format(text.count(' ')))
+
+# 3.	Посчитайте сколько у вас символов точки '.' в строке.
+print('\n\nTask3 result:')
+
+print("Space quantity: {0}".format(text.count('.')))
+
+#4.	Создайте строку "Homework". Преобразуйте ее в строку длиной 100 символов, посередине которой исходное слово,
+# а с обоих сторон строка заполнена пробелами. Выведите ее на экран.
+print('\n\nTask4 result:')
+
+hw = 'Homework'
+hw_new = (' ' * 46 + hw + ' ' * 46)
+print('var1: ', '|' + hw_new + '|', '<--length of this =', len(hw_new))
+
+hw2 = 'Homework'
+print('var2: ', '|', hw2.center(98, ' '), '|')
+
+# 5.	Сделайте первые буквы слов строки большими (upper case).
+print('\n\nTask5 result:')
+
+tas = 'qweryty asdfgh zxcvbnm'
+
+print(tas.partition(' '))
+
