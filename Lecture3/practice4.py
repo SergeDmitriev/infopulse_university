5693a2044064fffbfd64cd31e6173c82bdf6b0b3
s.dmitriev <s.dmitriev@e-tender.biz>
2018-04-09 00:11:00 +0300

    lecture 3 practice

--- Lecture3/practice4.py
+++ Lecture3/practice4.py
@@ -14,3 +14,18 @@ while True:
         print('Error:', e)
     else:
         break
+
+
+
+def find_gip(k1, k2):
+    """
+    :param k1:  katet 1 length
+    :param k2:  katet 2 length
+    :return:
+    """
+    from math import sqrt
+    c = sqrt(k1 ** 2 + k2 ** 2)
+    return c
+k1 = input('k1:')
+k2 = input('k2:')
+print(find_gip(4, 5))
