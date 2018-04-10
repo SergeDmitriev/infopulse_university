1adc8be341c39a64155195a8ff48e74e48287361
s.dmitriev <s.dmitriev@e-tender.biz>
2018-04-10 22:43:17 +0300

    task8 done

---
+++ Lecture3/homework/task8.py
@@ -0,0 +1,28 @@
+# Оформляем в функции наши задания на уроке:
+# 1)	Пишем функцию, которая попросит пользователя ввести слово (строка без пробелов в середине,
+# а вначале и в конце пробелы могут быть). Пока он не введёт правильно, просите его ввести. Функция
+# возвращает введённое слово
+print('task8.1: ')
+
+
+def enter_text(inp_text):
+    while ' ' in inp_text.strip():
+        inp_text = input('Enter your text here:')
+    return inp_text
+
+
+print(enter_text(input("Enter your text here:")))
+
+
+# 2)	Пишем функцию, которая попросит ввести число. Пока он не введёт правильно, просите его ввести. Функция
+# возвращает введённое число.
+print('\n' + 'task8.2: ')
+
+
+def enter_number(text):
+    while not text.isdigit():
+        text = input("Text not a number, pls enter correct:")
+    return text
+
+
+print(enter_number(input("Enter you number:")))
