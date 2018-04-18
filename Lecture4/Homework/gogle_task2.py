055f33a510e08d5ae8729fa50c9ae9471c16e1a6
s.dmitriev <s.dmitriev@e-tender.biz>
2018-04-18 23:08:18 +0300

    Lecture5.Homework google tasks1 and 2
    @elpiankova

--- Lecture4/Homework/gogle_task2.py
+++ Lecture4/Homework/gogle_task2.py
@@ -1,5 +1,3 @@
-#original file list1.py
-
 # # Google's Python Class
 # # http://code.google.com/edu/languages/google-python-class/
 #
@@ -20,21 +18,26 @@
 # # instead of the actual count.
 # # So donuts(5) returns 'Number of donuts: 5'
 # # and donuts(23) returns 'Number of donuts: many'
-# def donuts(count):
-#     # +++your code here+++
-#     return
-#
-#
-# # B. both_ends
-# # Given a string s, return a string made of the first 2
-# # and the last 2 chars of the original string,
-# # so 'spring' yields 'spng'. However, if the string length
-# # is less than 2, return instead the empty string.
-# def both_ends(s):
-#     # +++your code here+++
-#     return
-#
-#
+
+
+def donuts(count):
+    if count >= 10:
+        count = 'many'
+    return 'Number of donuts: {0}'.format(count)
+
+
+# B. both_ends
+# Given a string s, return a string made of the first 2
+# and the last 2 chars of the original string,
+# so 'spring' yields 'spng'. However, if the string length
+# is less than 2, return instead the empty string.
+def both_ends(s):
+    if len(s)<2:
+        return ''
+    else:
+        return s[:2] + s[-2:]
+
+
 # # C. fix_start
 # # Given a string s, return a string
 # # where all occurences of its first char have
@@ -44,62 +47,75 @@
 # # Assume that the string is length 1 or more.
 # # Hint: s.replace(stra, strb) returns a version of string s
 # # where all instances of stra have been replaced by strb.
-# def fix_start(s):
-#     # +++your code here+++
-#     return
-#
-#
-# # D. MixUp
-# # Given strings a and b, return a single string with a and b separated
-# # by a space '<a> <b>', except swap the first 2 chars of each string.
-# # e.g.
-# #   'mix', pod' -> 'pox mid'
-# #   'dog', 'dinner' -> 'dig donner'
-# # Assume a and b are length 2 or more.
-# def mix_up(a, b):
-#     # +++your code here+++
-#     return
-#
-#
-# # Provided simple test() function used in main() to print
-# # what each function returns vs. what it's supposed to return.
-# def test(got, expected):
-#     if got == expected:
-#         prefix = ' OK '
-#     else:
-#         prefix = '  X '
-#     print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
-#
-#
-# # Provided main() calls the above functions with interesting inputs,
-# # using test() to check if each result is correct or not.
-# def main():
-#     print('donuts')
-#     # Each line calls donuts, compares its result to the expected for that call.
-#     test(donuts(4), 'Number of donuts: 4')
-#     test(donuts(9), 'Number of donuts: 9')
-#     test(donuts(10), 'Number of donuts: many')
-#     test(donuts(99), 'Number of donuts: many')
-#
-#     print('\nboth_ends')
-#     test(both_ends('spring'), 'spng')
-#     test(both_ends('Hello'), 'Helo')
-#     test(both_ends('a'), '')
-#     test(both_ends('xyz'), 'xyyz')
-#
-#     print('\nfix_start')
-#     test(fix_start('babble'), 'ba**le')
-#     test(fix_start('aardvark'), 'a*rdv*rk')
-#     test(fix_start('google'), 'goo*le')
-#     test(fix_start('donut'), 'donut')
-#
-#     print('\nmix_up')
-#     test(mix_up('mix', 'pod'), 'pox mid')
-#     test(mix_up('dog', 'dinner'), 'dig donner')
-#     test(mix_up('gnash', 'sport'), 'spash gnort')
-#     test(mix_up('pezzy', 'firm'), 'fizzy perm')
-#
-#
-# # Standard boilerplate to call the main() function.
-# if __name__ == '__main__':
-#     main()
+
+
+def fix_start(s):
+    if len(s)>1:
+        result = s[0] + s[1:].replace(s[0], '*')
+        return result
+    else:
+        return ''
+
+
+
+
+# D. MixUp
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+
+
+def mix_up(a, b):
+    if len(a) and len(b) > 2:
+        st_a = a.replace(a[:2], b[:2])
+        st_b = b.replace(b[:2], a[:2])
+        return '{0} {1}'.format(st_a, st_b)
+    else:
+        return ''
+
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# Provided main() calls the above functions with interesting inputs,
+# using test() to check if each result is correct or not.
+def main():
+    print('donuts')
+    # Each line calls donuts, compares its result to the expected for that call.
+    # test(donuts(4), 'Number of donuts: 4')
+    # test(donuts(9), 'Number of donuts: 9')
+    # test(donuts(10), 'Number of donuts: many')
+    # test(donuts(99), 'Number of donuts: many')
+    #
+    # print('\nboth_ends')
+    # test(both_ends('spring'), 'spng')
+    # test(both_ends('Hello'), 'Helo')
+    # test(both_ends('a'), '')
+    # test(both_ends('xyz'), 'xyyz')
+    #
+    # print('\nfix_start')
+    # test(fix_start('babble'), 'ba**le')
+    # test(fix_start('aardvark'), 'a*rdv*rk')
+    # test(fix_start('google'), 'goo*le')
+    # test(fix_start('donut'), 'donut')
+
+    print('\nmix_up')
+    test(mix_up('mix', 'pod'), 'pox mid')
+    test(mix_up('dog', 'dinner'), 'dig donner')
+    test(mix_up('gnash', 'sport'), 'spash gnort')
+    test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+    main()
