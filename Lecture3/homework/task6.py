5c7ccb998b2e3f5bdca48e02e529afaf598b12d6
s.dmitriev <s.dmitriev@e-tender.biz>
2018-04-22 23:19:53 +0300

    edit lecture3 task6
    is_year_leaf method

--- Lecture3/homework/task6.py
+++ Lecture3/homework/task6.py
@@ -1,26 +1,28 @@
 # Это уже было, но теперь оформите это функцией:
 # 1)	Написать функцию is_year_leap, принимающую 1 аргумент — год, и возвращающую True,
 # если год високосный, и False иначе.
-print('taskl6.1: ')
+# print('taskl6.1: ')
 
 
 def is_year_leap(year):
     """
     :return: True, if year is leap
     """
-    if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
-        return False
-    else:
-        return True
+    try:
+        if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
+            return False
+        elif year == 0:
+            return 'Wrong year'
+        else:
+            return True
+    except :
+        return 'string values are not accepted!'
 
-
-y = int(input('Enter year number: '))
-print('Year is leap& ', is_year_leap(y))
 
 
 # 2)	Функция принимает три числа a, b, c. Функция должна определить, существует ли треугольник
 # с такими сторонами. Если треугольник существует, вернёт True, иначе False.
-print('\n' + 'taskl6.2: ')
+# print('\n' + 'taskl6.2: ')
 
 
 def if_triangle_exists(a, b, c):
@@ -30,7 +32,14 @@ def if_triangle_exists(a, b, c):
         return False
 
 
-s1 = float(input('a= '))
-s2 = float(input('b= '))
-s3 = float(input('c= '))
-print('Can exists? ', if_triangle_exists(s1, s2, s3))
+if __name__ == '__main__':
+
+    # print('taskl6.1: ')
+    y = input('Enter year: ')
+    print('Year is leap ', is_year_leap(y))
+
+    print('\n' + 'taskl6.2: ')
+    s1 = float(input('a= '))
+    s2 = float(input('b= '))
+    s3 = float(input('c= '))
+    print('Can exists? ', if_triangle_exists(s1, s2, s3))
